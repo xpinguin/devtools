c3837947b71ce9d2703b1d29aaaefa950d418b0c
xpinguin <xpinguin@users.noreply.github.com>
2019-06-23 13:37:02 +0300

    numeral pkg skel + snippets dir

---
+++ devtools/multicat-numeral/numeral.go
@@ -0,0 +1,3 @@
+/*compound numeral (\in N{0..sup}:N{1..sup}) to explicitly label: data types+instances (eg. map,json,struct,term,tree(_AST_,XML,...)) and computation (eg. SSA-instruction, _AST_, principal term)"
+[master 7316826] init[nocode,task,todo]: compound numeral (\in N{0..sup}:N{1..sup}) to explicitly label: data types+instances (eg. map,json,struct,term,tree(_AST_,XML,...)) and computation (eg. SSA-instruction, _AST_, principal term)*/
+package numeral
