5038bd6d8b8b9dd8f8b98f516d098b03f7a5fcd0
xpinguin <xpinguin@users.noreply.github.com>
2019-06-23 18:36:31 +0300

    pretty-printed go struct numeration: depth:pos  //total_post   **indent_cardinality

--- devtools/multicat-numeral/numeral.go
+++ devtools/multicat-numeral/numeral.go
@@ -3,6 +3,7 @@
 package numeral
 
 import (
+	"fmt"
 	"regexp"
 	"strings"
 )
@@ -23,7 +24,7 @@ var srcStructLn = regexp.MustCompile(
 )
 
 //////////////////////
-func stripStructSrc(structSrc string) (src string, indentCnt map[string]int) {
+func StripStructSrc(structSrc string) (src string, indentCnt map[string]int) {
 	structSrc = strings.TrimSpace(structSrc)
 	structSrc = strings.ReplaceAll(structSrc, "\r\n", "\n")
 	structLines := strings.Split(structSrc, "\n")
@@ -31,7 +32,7 @@ func stripStructSrc(structSrc string) (src string, indentCnt map[string]int) {
 	indentCnt = map[string]int{}
 	for _, line := range structLines {
 		stripped := srcStructLn.ReplaceAllString(line, "$1$4")
-		src += stripped + "\n"
+		src += strings.TrimRight(stripped, " }\t{") + "\n"
 
 		indent := whitespacePfxLn.ReplaceAllString(stripped, "$1")
 		indentCnt[indent] += 1
@@ -39,4 +40,68 @@ func stripStructSrc(structSrc string) (src string, indentCnt map[string]int) {
 	}
 
 	return src, indentCnt
+}
+
+//////////////////////
+/// ?HINT: numerator/denominator <=> rational
+/////////////////////
+func NumerateStructSrc(structSrc string) (nsrc string) {
+	structSrc, indents := StripStructSrc(structSrc)
+	structLines := strings.Split(structSrc, "\n")
+
+	/// FIXME
+	lineTypeOffs := 0
+	if strings.HasPrefix(strings.TrimSpace(structLines[0]), "type") {
+		lineTypeOffs = 1
+	}
+	////
+	delete(indents, "") // module-level
+	////
+	for pfx, c := range indents {
+		if strings.ContainsAny(pfx, " ") { // NB. space and \t could repr different cardinality, eg ' ' = \t div 4 (or 2, or 8, you see, don't you?)
+			nsrc += fmt.Sprintf("//!!! XXX: NOT IMPLEMENTED: Rational sub-cardinality: '%s'. Opaque cardinality: %d\n", pfx, c)
+		}
+	}
+	////
+	if nsrc != "" {
+		nsrc += "\n\n"
+	}
+	////
+
+	// or 'denominator' instead???
+	var lastDepth int
+	depthFieldPos := map[int]uint{}
+	/// stupid pre-init, just to be sure
+	for d := 0; d <= len(indents); d++ {
+		depthFieldPos[d] = 1
+	}
+	///
+
+	for numerator, line := range structLines[lineTypeOffs:] {
+		var nline string
+
+		for pfx, cardinality := range indents {
+			if constrs := strings.TrimPrefix(line, pfx); constrs == strings.TrimLeft(line, "\t") {
+				depth := len(pfx)
+				if depth < lastDepth {
+					depthFieldPos[lastDepth] = 1
+				}
+				if depth != lastDepth {
+					lastDepth = depth
+				}
+				/////
+				nline = fmt.Sprintf("%d:%d\t//%d\t|%s\t**%d", len(pfx), depthFieldPos[depth], numerator+1, constrs, cardinality)
+				/////
+				depthFieldPos[depth] += 1
+				break
+			}
+		}
+
+		if nline != line {
+			nsrc += nline + "\n"
+		} else if line != "" {
+			nsrc += "%% " + nline + "\n"
+		}
+	}
+	return nsrc
 }
