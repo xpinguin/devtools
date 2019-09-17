557580c1d567d23fc6506759df2ff90e37a409db
Vladimir S <xpinguin@users.noreply.github.com>
2019-09-17 07:03:14 +0300

    more reasonable .proto-file constructor

--- cmd/idlc/proto_form.go
+++ cmd/idlc/proto_form.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 //go:generate go get -v golang.org/x/tools/cmd/stringer
@@ -23,11 +24,15 @@ const (
 
 	REPEATED NameAfx = 1 << iota
 	ONE_OF
-
-	ENUM
-	VARIANT
-
 	DIRECTIVE
+	SERVICE
+	MESSAGE
+	ENUM
+	FIELD
+	VARIANT
+	RPC
+
+	METHOD = RPC
 	OPTION = DIRECTIVE
 )
 
@@ -37,7 +42,6 @@ type TypeAfx int
 
 const (
 	NONE TypeAfx = iota
-
 	BOOL TypeAfx = 1 << iota
 	STRING
 	BYTES
@@ -45,43 +49,158 @@ const (
 	INT64
 	INT
 	FLOAT
-
 	PB_ANY
+	//PB_MESSAGE
 	PB_TIMESTAMP
 	PB_DURATION
 )
 
 ///////////////////////////
-type Head struct {
-	flags NameAfx
-	name  string
-}
-
+/** REM it is somewhat helpful to think about "terminal" & "non-terminal" fields */
 type Field struct {
-	*Head
-	typ TypeAfx
-	tag int
-}
-
-type Block struct {
-	*Head
-	fields []Field
+	flags NameAfx
+	typ   TypeAfx
+	name  string
+	tag   int
 }
 
 ///////////////////////////
 type Message struct {
-	*Block
+	/*head*/ *Field // NB. untagged field semantically
+	/*body*/ ents []*Field
+}
+
+///////////////////////////
+type Method struct {
+	/*label*/ *Field
+	arg, ret *Message
 }
 
 type Service struct {
-	*Block
+	/*label*/ *Field
+	meths []*Method
 }
 
-type Enum struct {
-	*Block
+///////////////////////////
+///////////////////////////
+type ProtoPkg struct {
+	name, qualName string
+
+	svcs []*Service
+	msgs []*Message
+
+	opts map[string]string
 }
 
-///// TEST CODE
+///////////////////////////
+///////////////////////////
+func (f *Field) Name() (res string) {
+	flags := f.flags.String()
+	////
+	if f.typ == NONE {
+		return strings.Join([]string{flags, f.name}, " ")
+	}
+	////
+	typ := f.typ.String()
+	return strings.Join([]string{flags, typ, f.name}, " ")
+}
+
+func (msg *Message) Name() string {
+	return msg.Field.name
+}
+
+///////////////////////////
+///////////////////////////
+func (f *Field) String() string {
+	flags := f.flags & ^FIELD
+	////
+	if flags|FIELD == f.flags {
+		return fmt.Sprintf("%s = %d",
+			strings.Join([]string{flags.String(), f.typ.String(), f.name}, " "),
+			f.tag,
+		)
+	}
+	return strings.Join([]string{flags.String(), f.name}, " ")
+}
+
+func (meth *Method) String() (res string) {
+	res = meth.Field.String()
+	if meth.arg != nil {
+		res += fmt.Sprintf("(%s)", meth.arg.Name())
+	}
+	if meth.ret != nil {
+		res += fmt.Sprintf(" returns (%s)", meth.ret.Name())
+	}
+	return res
+}
+
+func (svc *Service) String() (res string) {
+	res = svc.Field.String() + " {\n"
+	for _, m := range svc.meths {
+		res += "    " + m.String() + ";\n"
+	}
+	return res + "}\n"
+}
+
+func (msg *Message) String() (res string) {
+	res = msg.Field.String() + " {\n"
+	for i, ent := range msg.ents {
+		if ent.tag == 0 {
+			ent.tag = i*10 + ent.tag%10
+		}
+		res += "    " + ent.String() + ";\n"
+	}
+	return res + "}\n"
+}
+
+///////////////////////////
+/////////////////////////// TEST CODE
 func main() {
-	fmt.Println(NameAfx(2), TypeAfx(4))
+	//////////////
+	msgs := []*Message{
+		&Message{
+			Field: &Field{name: "SearchByRegionRequest", flags: MESSAGE},
+			ents: []*Field{
+				&Field{typ: INT32, name: "region_id", tag: 1, flags: FIELD},
+				&Field{name: "SearchRequest q", tag: 2, flags: FIELD},
+			}},
+
+		&Message{
+			Field: &Field{name: "SearchByHotelsRequest", flags: MESSAGE},
+			ents: []*Field{
+				&Field{typ: STRING, name: "hotels_ids", tag: 1, flags: FIELD | REPEATED},
+				&Field{name: "SearchRequest q", tag: 2, flags: FIELD},
+			}},
+
+		&Message{
+			Field: &Field{name: "SearchResponse", flags: MESSAGE},
+			ents: []*Field{
+				&Field{name: "Component components", tag: 1, flags: FIELD | REPEATED},
+				&Field{name: "Accommodation components_metadata", tag: 2, flags: FIELD | REPEATED},
+				&Field{typ: STRING, name: "error", tag: 10, flags: FIELD},
+			}},
+	}
+
+	//////
+	SearchByRegion := &Method{
+		Field: &Field{name: "SearchByRegion", flags: METHOD},
+		arg:   msgs[0],
+		ret:   &Message{Field: &Field{name: "SearchResponse", flags: MESSAGE}}, // NB. that's won't work...
+	}
+	SearchByHotels := &Method{
+		Field: &Field{name: "SearchByHotels", flags: METHOD},
+		arg:   msgs[1],
+		ret:   &Message{Field: &Field{name: "SearchResponse", flags: MESSAGE}},
+	}
+
+	svc1 := &Service{
+		Field: &Field{name: "Accommodations", flags: SERVICE},
+		meths: []*Method{SearchByRegion, SearchByHotels},
+	}
+	////
+	fmt.Printf("-----\n%v\n---------\n", svc1.String())
+
+	for _, m := range msgs {
+		fmt.Println(m.String())
+	}
 }
